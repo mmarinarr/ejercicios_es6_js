78e5fea7a37d204cb524394b3d3b4a509684ecd4
mmarinarr <marina10paula5@gmail.com>
2024-10-25 17:43:46 +0200

    update

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,8 +1,14 @@
 //4.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean mayor que 18
 const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
+const filteredAges = ages.filter(ages => ages > 18);
+console.log(filteredAges);
+
 //4.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean par.
 const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const evenAges = ages2.filter(ages2 => ages2 % 2 === 0);
+console.log(evenAges); 
 
 //4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que tengan el gameMorePlayed = 'League of Legends'.
 const streamers = [
@@ -12,6 +18,11 @@ const streamers = [
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
 
+const lolStreamers = streamers
+    .filter(streamer => streamer.gameMorePlayed === 'League of Legends') 
+
+console.log(lolStreamers);
+
 //4.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos usar la funcion .includes() para la comprobación.
 const streamers2 = [
 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
@@ -20,4 +31,20 @@ const streamers2 = [
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
 
+const uStreamers = streamers2
+    .filter(streamers2 => streamers2.name.includes('u')) 
+
+console.log(uStreamers);
+
 //4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion .includes() para la comprobación.Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando .age sea mayor que 35.
+
+const lolStreamers2 = streamers2
+    .filter(streamer => streamer.gameMorePlayed.includes('Legends')) 
+    .map(streamer => {
+        if (streamer.age > 35) {
+            return { ...streamer, gameMorePlayed: streamer.gameMorePlayed.toUpperCase() };
+        }
+        return streamer; 
+    });
+
+console.log(lolStreamers2);
